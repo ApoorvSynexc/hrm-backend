29f0a3e1b5aa6d4759d9592f142bd7f2201a997c
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-04-13 15:36:12 +0530

    initial commit

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('Database connection established...');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}

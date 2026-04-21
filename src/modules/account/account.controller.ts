74aea03611af68048ac4bf59504e0d33a2791627
Apoorv Saxena <apoorv.saxena@synexc.com>
2026-04-21 18:13:42 +0530

    [Update]
    role and permission

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -37,6 +37,8 @@ export class AccountController {
     @CurrentUser() current: JwtPayload,
     @Body() dto: UpdateUserDto,
   ) {
+    console.log({current});
+    
     const user = await this.accountService.updateUser(current.sub, dto);
     return { message: 'common.updated', data: user };
   }
